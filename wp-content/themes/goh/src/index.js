32a351f7ac077908f626f90f226545acd68672b8
Dick Törnfeldt <dicktornfeldt@Dicks-MacBook-Pro.local>
2019-06-11 23:48:51 +0200

    update plugins and initial setups

--- wp-content/themes/goh/src/index.js
+++ wp-content/themes/goh/src/index.js
@@ -1,23 +1,20 @@
-import jQuery from 'jquery';
-import './style.scss';
+import jQuery from "jquery";
+import "./style.scss";
 
-import Router from './javascripts/util/Router';
-import common from './javascripts/routes/common';
-import home from './javascripts/routes/home';
-import about from './javascripts/routes/about';
+import Router from "./javascripts/util/Router";
+import common from "./javascripts/routes/common";
+import home from "./javascripts/routes/home";
+import mittKonto from "./javascripts/routes/my-account";
 
 /**
  * Populate Router instance with DOM routes
  * @type {Router} routes - An instance of our router
  */
 const routes = new Router({
-  /** All pages */
   common,
-  /** Home page */
   home,
-  /** About Us page, note the change from about-us to aboutUs. */
-  about
+  mittKonto
 });
 
-/** Load Events */
+// Load Events
 jQuery(document).ready(() => routes.loadEvents());
