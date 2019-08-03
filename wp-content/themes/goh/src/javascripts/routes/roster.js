ca2a4f69ac6d7917657ce6b6fa401c764f81d63f
Dick Törnfeldt <dicktornfeldt@Dicks-MacBook-Pro.local>
2019-08-03 13:34:48 +0200

    added filter of raiding members

--- wp-content/themes/goh/src/javascripts/routes/roster.js
+++ wp-content/themes/goh/src/javascripts/routes/roster.js
@@ -4,7 +4,7 @@ import List from "list.js";
 export default {
   init() {
     var options = {
-      valueNames: ["nick", "race", "class", "role"]
+      valueNames: ["raiders", "nick", "rank", "race", "class", "role"]
     };
 
     // Init list
@@ -12,6 +12,29 @@ export default {
 
     rosterList.sort("class", {
       order: "asc"
+    });
+
+    // checkbox filter
+    var activeFilters = [];
+
+    //filter
+    $(".js-filter-member").change(function() {
+      var value = $(this).val();
+
+      if (value === "raiders") {
+        //  add to list of active filters
+        activeFilters.push(value);
+      } else {
+        // remove from active filters
+        activeFilters.splice(activeFilters.indexOf(value), 1);
+      }
+
+      rosterList.filter(function(item) {
+        if (activeFilters.length > 0) {
+          return activeFilters.indexOf(item.values().raiders) > -1;
+        }
+        return true;
+      });
     });
   },
   finalize() {
