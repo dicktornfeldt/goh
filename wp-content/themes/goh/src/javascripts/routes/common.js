11567f111d13130bc942b3ae327635f96e77bfbb
Dick Törnfeldt <dicktornfeldt@Dicks-MBP.lan>
2019-08-11 21:57:43 +0200

    anchor to news

--- wp-content/themes/goh/src/javascripts/routes/common.js
+++ wp-content/themes/goh/src/javascripts/routes/common.js
@@ -1,6 +1,11 @@
+import $ from "jquery";
+
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    $(".js-anchorlink").click(function() {
+      var aid = $(this).attr("href");
+      $("html,body").animate({ scrollTop: $(aid).offset().top }, "slow");
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
